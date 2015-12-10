a04ea2dec1413765fa2153cd60a40c4c423f4b74
Stefan Keil <an.stefan.keil@googlemail.com>
2015-12-10 16:26:55 +0100

    added basic user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,9 @@ var Schema       = mongoose.Schema
 
 // define the model properties
 var UserSchema   = new Schema({
+	name:     String,
+	email:    String,
+	password: String
 })
 
 module.exports = mongoose.model('User', UserSchema)
